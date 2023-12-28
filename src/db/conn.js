682e7f9b01de99d847b071bbb77d8f3abdc370b0
kaushik0310 <kaushik1010@gmail.com>
2023-12-28 12:53:53 +0530

    testing commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/youtuberegistration")
 .then(()=>{
-    console.log(`connection successful`); 
+    console.log(`connection sskddkncgit successful`); 
 }).catch((e)=>{
     console.log(`no connection`);
 
